07bdf7589a96b140a5827e8dd8253273dd43c0b7
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-12 21:45:20 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/pyC98UU ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+
+
+client.login(process.env.BOT_TOKEN);
